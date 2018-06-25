500fffa4d65dd77c80e6a5e8b33ffe466782e2db
ramourah <r.amourah@gmail.com>
2018-06-25 13:47:29 -0700

    Finished number guessing game version 1

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,1 +1,26 @@
 /*eslint-env browser*/
+function playGame() {
+alert(("Select a low number and a high number. Then, select a number between them."));
+
+//get the low and high bounds
+//use parseInt() to make sure we have numbers, not strings
+var from = parseInt(prompt("Enter the lower bound."));
+var to = parseInt(prompt("Enter the higher bound."));
+//get integer bwteen [from , to]. Math.random() returns decimals, use Math.round() to get whole numbers
+var target = Math.round(Math.random() * (to - from) + from);
+var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + to));
+var totalGuesses = 1;
+//loop until user guesses correctly
+while(currentGuess != target){
+    if(currentGuess < target){
+        currentGuess = parseInt(prompt("Enter a higher number."));
+        
+        totalGuesses++
+    }else if(currentGuess > target){
+        currentGuess = parseint(prompt("Enter a lower number."));
+        
+        totalGuesses++
+    }
+    }
+    alert("It took " + totalGuesses + " tries to guess the correct number.");
+}
